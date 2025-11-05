61e7c52e31ef3b996455e67bbab475297a60db65
243691Heber <243691@ids.upchiapas.edu.mx>
2025-11-05 13:29:06 -0600

    Nav Manger

---
+++ app/src/main/java/com/heber/practica1/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.heber.practica1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.heber.practica1.navManager.NavManager
+import com.heber.practica1.ui.theme.Practica1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica1Theme {
+                NavManager()
+            }
+
+        }
+    }
+}
+@Composable
+@Preview
+fun PreviewMainActivity(){
+    Practica1Theme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Hola Mundo",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/heber/practica1/data/Student.kt
@@ -0,0 +1,7 @@
+package com.heber.practica1.data
+
+data class  Student(
+     val id: Long,
+    val name: String,
+    val description: String
+)
